aeef7c26d6a7b4cf5dd24787d91aa7de08f48252
dabiegorden <117079883+dabiegorden@users.noreply.github.com>
2023-07-23 16:01:29 -0700

    first commit

---
+++ sanityeccommerce/sanity.cli.js
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: 'saz8mtn2',
+    dataset: 'production'
+  }
+})
---
+++ sanityeccommerce/schemas/index.js
@@ -0,0 +1,5 @@
+import product from './product';
+import banner from './banner';
+
+//use our schema
+export const schemaTypes = [ product, banner ];
